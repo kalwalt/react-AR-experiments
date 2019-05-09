075a9d054b17af11d78fe1ba71799f5c80e1bbda
kalwalt <github@kalwaltart.it>
2019-05-09 17:35:21 +0200

    first commit with Three.js and THREEAR

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const THREE = require('three');
+//const AR = require('threear');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  render(){
+    const AR = require('threear');
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    function animate() {
+	     requestAnimationFrame( animate );
+	      renderer.render( scene, camera );
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+      }
+    animate();
+  return(
+  <div></div>
+  )
+  }
 }
 
 export default App;
