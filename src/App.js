013852bbdb3f942c1a03b5f1376bc4ec892f241d
kalwalt <github@kalwaltart.it>
2019-05-13 10:15:39 +0200

    update to threear v1.0.0-alpha.4 and this works

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
 import React from 'react';
 import './App.css';
 const THREE = require('three');
-//const AR = require('threear');
+const THREEAR = require('threear');
 
 
 class App extends React.Component {
 
   render(){
-    const AR = require('threear');
 
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
 
     camera.position.z = 5;
 
-    var renderer = new THREE.WebGLRenderer();
+    var renderer = new THREE.WebGLRenderer({
+				// antialias	: true,
+				alpha: true
+			});
+		renderer.setClearColor(new THREE.Color('lightgrey'), 0)
     renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.domElement.style.position = 'absolute'
+		renderer.domElement.style.top = '0px'
+		renderer.domElement.style.left = '0px'
     document.body.appendChild( renderer.domElement );
 
-    function animate() {
-	     requestAnimationFrame( animate );
+    var markerGroup = new THREE.Group();
+		scene.add(markerGroup);
+
+    var source = new THREEAR.Source({ renderer, camera });
+
+    THREEAR.initialize({ source: source }).then((controller) => {
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshNormalMaterial();
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        markerGroup.add(cube)
+        var path = './data/patt.hiro';
+        var patternMarker = new THREEAR.PatternMarker({
+    					patternUrl: path,
+    					markerObject: markerGroup
+    				});
+  	    controller.trackMarker(patternMarker);
+
+    requestAnimationFrame(function animate(nowMsec){
+        // measure time
+				lastTimeMsec = lastTimeMsec || nowMsec-1000/60;
+				var deltaMsec = Math.min(200, nowMsec - lastTimeMsec);
+				lastTimeMsec = nowMsec;
 	      renderer.render( scene, camera );
+        controller.update( source.domElement );
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
-      }
-    animate();
+        // keep looping
+			  requestAnimationFrame( animate );
+        });
+
+    });
   return(
   <div></div>
   )
