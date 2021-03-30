6ac5df6bc24ecfe7981a13a443062774f0d1d083
kalwalt <github@kalwaltart.it>
2021-03-30 13:22:56 +0200

    with aframe-react start webcam stream

--- react-arjs-example/src/ARjsComp.js
+++ react-arjs-example/src/ARjsComp.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Entity, Scene} from 'aframe-react';
+import 'babel-polyfill';
 import 'aframe'
 import 'arjs'
 
@@ -13,12 +15,14 @@ export default class ARjsComp extends React.Component {
           style={{ width: "800px", height: "800px" }}
           ref={mount => { this.mount = mount}}
       >
+      <Scene>
        <a-scene embedded arjs>
          <a-marker preset="hiro">
            <a-cone position='0 0.5 0' rotation="0 0 0" radius-bottom="0.5" material='opacity: 0.75; color: red;'></a-cone>
          </a-marker>
        <a-entity camera></a-entity>
        </a-scene>
+       </Scene>
       </div>
     )
   }
